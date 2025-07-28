ef261bdd960765a285162cce27e2dc33bceadc49
Bart Kuipers <bart@touchtribe.nl>
2025-07-28 16:19:44 +0200

    Revert "set attributes on read to make ignore_changes lifecycle property work"
    This reverts commit 713dab1f7fd8cda326d5b6788faef97dbdddcb4e.

--- commercelayer/resource_payment_method.go
+++ commercelayer/resource_payment_method.go
@@ -160,32 +160,13 @@ func resourcePaymentMethodReadFunc(ctx context.Context, d *schema.ResourceData, 
 		return diagErr(err)
 	}
 
-	paymentMethod, ok := resp.GetDataOk()
+	address, ok := resp.GetDataOk()
 	if !ok {
 		d.SetId("")
 		return nil
 	}
 
-	d.SetId(paymentMethod.GetId().(string))
-
-	attrs := map[string]interface{}{
-		"name":                          paymentMethod.Attributes.GetName(),
-		"payment_source_type":           paymentMethod.Attributes.GetPaymentSourceType(),
-		"currency_code":                 paymentMethod.Attributes.GetCurrencyCode(),
-		"moto":                          paymentMethod.Attributes.GetMoto(),
-		"price_amount_cents":            paymentMethod.Attributes.GetPriceAmountCents(),
-		"require_capture":               paymentMethod.Attributes.GetRequireCapture(),
-		"auto_place":                    paymentMethod.Attributes.GetAutoPlace(),
-		"auto_capture":                  paymentMethod.Attributes.GetAutoCapture(),
-		"auto_capture_max_amount_cents": paymentMethod.Attributes.GetAutoCaptureMaxAmountCents(),
-		"reference":                     paymentMethod.Attributes.GetReference(),
-		"reference_origin":              paymentMethod.Attributes.GetReferenceOrigin(),
-		"metadata":                      paymentMethod.Attributes.GetMetadata(),
-	}
-
-	if err := d.Set("attributes", []interface{}{attrs}); err != nil {
-		return diagErr(err)
-	}
+	d.SetId(address.GetId().(string))
 
 	return nil
 }
